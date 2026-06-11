9c71d0a1d704e1df6173db3dbe70c1072ce8235a
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-11 13:48:39 -0300

    Deleting Department view controller

--- Models/ViewModels/Department.cs
+++ Models/ViewModels/Department.cs
@@ -1,9 +1,8 @@
-﻿namespace SalesWebMvc.Models
+﻿namespace SalesWebMvc.Models.ViewModels
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
     }
 }
