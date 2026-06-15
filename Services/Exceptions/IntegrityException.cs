8559862f72f3d91a188b88ba20e6daaacf920068
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-15 16:52:04 -0300

    Exception handling for delete (referential integrity)

---
+++ Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message)
+            :base(message)
+        {
+
+        }
+    }
+}
