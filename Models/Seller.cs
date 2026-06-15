c5d47100e930294947007d9c7ef75a6e29a635ca
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-15 15:24:29 -0300

    Validation

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -7,15 +7,22 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(60 , MinimumLength = 3 ,ErrorMessage = "{0} Name size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.0, 500000.0 , ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         public double BaseSalary { get; set; }
 
