831c7168f50b1ceca635b365384a0af53a6992e7
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-15 16:41:40 -0300

    Asynchronous operations using Tasks (async, await)

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -28,7 +28,7 @@ namespace SalesWebMvc.Models
 
         public int DepartmentId { get; set; }
 
-        public Department Department { get; set; }
+        public Department? Department { get; set; }
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
